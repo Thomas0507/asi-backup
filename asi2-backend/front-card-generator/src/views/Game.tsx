69e1917c980b9f9483fc88677e9d69c689b3d619
Thomas0507 <marin.f.thomas@gmail.com>
2024-11-18 23:05:46 +0100

    socket listener on front view + express socket

--- asi2-backend/front-card-generator/src/views/Game.tsx
+++ asi2-backend/front-card-generator/src/views/Game.tsx
@@ -1,3 +1,93 @@
+import { socket } from "../socket";
+import { useState } from "react";
 export default function Game() {
-    
+  socket.connect();
+  const [gameSession] = useState({
+    player1Name: "player-1",
+    player2Name: "player-2",
+    player1Cards: [
+      {
+        name: "card-1",
+        attack: 10,
+        defense: 5,
+        image:
+          "https://i.etsystatic.com/48925405/r/il/fdff31/5706371804/il_1140xN.5706371804_f3es.jpg",
+      },
+      {
+        name: "card-2",
+        attack: 12,
+        defense: 3,
+        image:
+          "https://i.etsystatic.com/48925405/r/il/fdff31/5706371804/il_1140xN.5706371804_f3es.jpg",
+      },
+      ,
+      {
+        name: "card-3",
+        attack: 5,
+        defense: 15,
+        image:
+          "https://i.etsystatic.com/48925405/r/il/fdff31/5706371804/il_1140xN.5706371804_f3es.jpg",
+      },
+    ],
+    player2Cards: [
+      {
+        name: "card-4",
+        attack: 10,
+        defense: 5,
+        image:
+          "https://i.etsystatic.com/48925405/r/il/fdff31/5706371804/il_1140xN.5706371804_f3es.jpg",
+      },
+      {
+        name: "card-5",
+        attack: 12,
+        defense: 3,
+        image:
+          "https://i.etsystatic.com/48925405/r/il/fdff31/5706371804/il_1140xN.5706371804_f3es.jpg",
+      },
+      ,
+      {
+        name: "card-6",
+        attack: 5,
+        defense: 15,
+        image:
+          "https://i.etsystatic.com/48925405/r/il/fdff31/5706371804/il_1140xN.5706371804_f3es.jpg",
+      },
+    ],
+  });
+
+  const rows = gameSession.player1Cards.map((card, i) => (
+    <div key={i} className="game-card-container">
+      <div className="game-title-wrapper">{card?.name}</div>
+      <div className="game-image-wrapper">
+        <img
+          className="game-image"
+          src={card?.image}
+          width="400px"
+          height="400px"
+          alt={card?.name}
+        ></img>
+      </div>
+      <div className="game-image-wrapper"></div>
+      <div className="game-pv-wrapper">PV</div>
+      <div className="game-stat-wrapper">
+        <div>{card?.attack} ATK</div>
+        <div>{card?.defense} DEF</div>
+      </div>
+    </div>
+  ));
+
+  return (
+    <div
+      className="game-wrapper"
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        alignContent: "center",
+        backgroundColor: "lightgrey",
+        padding: "2em",
+      }}
+    >
+      {rows}
+    </div>
+  );
 }
--- express-socket/src/index.ts
+++ express-socket/src/index.ts
@@ -2,46 +2,42 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
-
-
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 const mongoDBURL = "mongodb://127.0.0.1:27017/socket-game?authSource=admin";
 dotenv.config();
 
+const cors = require("cors");
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  // options
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
 
+io.on("connection", (socket) => {
+  console.log("user connected: ", socket.id);
+  socket.on("send_message", (data) => {
+    console.log("Message Received ", data); // Log the received message data
 
-app.use(express.json());
+    // Emit the received message data to all connected clients
+    io.emit("receive_message", data);
+  });
+});
+httpServer.listen(3000);
 
 // mongoose
-//   .connect(mongoDBURL, {}).then(
-//     console.log("connexion successful")
-//   ).catch((err: any) => {
-//     console.log("connexion failed", err)
-//     process.exit(-1);
+//   .connect(mongoDBURL, {})
+//   .then(() => {
+//     console.log("Connection Successfull");
+//   })
+//   .catch((err: any) => {
+//     console.log(err, "Received an Error");
 //   });
-
-io.on('connection', (socket: any) => {
-  console.log('a user connected', socket);
-});
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
-
-mongoose.connect(mongoDBURL, {
-})
-  .then(() => { console.log("Connection Successfull") })
-  .catch((err: any) => { console.log(err, "Received an Error") })
